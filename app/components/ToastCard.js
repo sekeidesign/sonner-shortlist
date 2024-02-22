09429a26eb90abc3516538a2643ee97684b98b64
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-02-22 16:38:37 -0500

    Add missing files

---
+++ app/components/ToastCard.js
@@ -0,0 +1,19 @@
+'use client';
+
+import Image from 'next/image';
+
+export default function ToastCard({ item }) {
+  return (
+    <div className="bg-white rounded-md w-64 overflow-hidden shadow-lg">
+      <img
+        src={item.image}
+        alt={item.title}
+        className="w-full h-20 object-cover rounded-t-sm"
+      />
+      <div className="p-6">
+        <h2 className="text-sm font-medium">{item.title}</h2>
+        <p className="text-xs mt-2 text-gray-500">{item.description}</p>
+      </div>
+    </div>
+  );
+}
---
+++ app/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    title: 'Catering 1',
+    description: "The best Polish food you'll ever eat!",
+    link: 'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+    image: 'https://source.unsplash.com/600x400/?catering',
+  },
+  {
+    title: 'Catering 2',
+    description:
+      'Tasty Mexican and Latin American catering for all your events',
+    link: 'https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app',
+    image: 'https://source.unsplash.com/600x400/?mexico',
+  },
+  {
+    title: 'Catering 3',
+    description: 'Delicious Indian food for all your events!',
+    link: 'https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+    image: 'https://source.unsplash.com/600x400/?italian-food',
+  },
+  {
+    title: 'Catering 4',
+    description: 'The best Italian food you will ever eat!',
+    link: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+    image: 'https://source.unsplash.com/600x400/?food',
+  },
+];
