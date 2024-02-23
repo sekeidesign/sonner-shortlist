5f53c5f8e0fab48f6cb9edf65edb3a14937cb6e7
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-02-23 07:43:21 -0500

    Add missing file

---
+++ app/components/Card.js
@@ -0,0 +1,23 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+export default function ToastCard({ item, isTiny, onEvent }) {
+  return (
+    <div
+      className={`bg-white rounded-md mx-auto overflow-hidden cursor-pointer hover:bg-gray-50 w-full`}
+    >
+      <img
+        src={item.image}
+        alt={item.title}
+        className={`w-full object-cover h-12`}
+      />
+      <div className={`${isTiny ? 'p-4' : 'p-3'}`}>
+        <h2 className="text-sm font-medium">{item.title}</h2>
+        <p className="text-xs mt-1 text-gray-500 line-clamp-2">
+          {item.description}
+        </p>
+      </div>
+    </div>
+  );
+}
